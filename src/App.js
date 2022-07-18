d5396ec3665f62788f267a592d5af457ff5cc8c6
Abdallah Sow <abdallahsow700@gmail.com>
2022-07-18 19:52:34 +0300

    New Website

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://codewars.com"
+          href="https://scrimba.com"
           target="_blank"
           rel="noopener noreferrer"
         >
