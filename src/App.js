de494ac9af6dd55643450f52d000aab70beda248
Abdallah Sow <abdallahsow700@gmail.com>
2022-08-03 22:47:41 +0300

    updated app.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="React's app logo" />
         <p>
-          Start coding your future
+          Break the coding interview with React.
         </p>
         <a
           className="App-link"
-          href="https://scrimba.com"
+          href="https://leetcode.com"
           target="_blank"
           rel="noopener noreferrer"
         >
