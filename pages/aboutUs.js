af8e27bfdf00ded283150eb6f8c99cb67bf3bfd7
Abdallah Sow <abdallahsow700@gmail.com>
2022-11-03 20:29:11 +0300

    created the Layout component

---
+++ pages/aboutUs.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Head>
+        <title>About Next</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default AboutUs;
