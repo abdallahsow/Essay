e4a03ee9d76949fe8dd7396b4eb94fbda9305c6e
Abdallah Sow <abdallahsow700@gmail.com>
2022-11-04 15:40:36 +0300

    updated the ArticleItem component

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
 import styles from "../styles/Articles.module.css";
 
 export default function ArticleItem({ article }) {
-  return <h3 className={styles.card}>{article.title}</h3>;
+  return (
+    <Link href="/article/[id]" as={`/article/${article.id}`} legacyBehavior>
+      <a className={styles.card}>
+        <h3>{article.title} &rarr;</h3>
+        <p>{article.body}</p>
+      </a>
+    </Link>
+  );
 }
