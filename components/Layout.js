934cb4cc3a056a21b8f9f74fa2ac2d12bed832a6
Abdallah Sow <abdallahsow700@gmail.com>
2022-11-04 12:08:54 +0300

    created the Header component

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Nav from "./Nav";
+import Header from "./Header";
 import styles from "../styles/Layout.module.css";
 
 export default function Layout({ children }) {
@@ -6,6 +7,7 @@ export default function Layout({ children }) {
     <>
       <Nav />
       <div className={styles.container}>
+        <Header />
         <main className={styles.main}>{children}</main>
       </div>
     </>
